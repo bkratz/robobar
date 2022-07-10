02f827a702efd6d01b35ac8fcb1139e724561a32
Birgit Kratz <mail@birgitkratz.de>
2022-07-10 22:06:28 +0200

    initial commit

---
+++ src/main/java/de/birgitkratz/robobar/RobobarApplication.java
@@ -0,0 +1,13 @@
+package de.birgitkratz.robobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobobarApplication.class, args);
+	}
+
+}
